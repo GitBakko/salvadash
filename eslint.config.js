cc78a4ac94697f6a81722974858a281687cbceb8
s.brunelli <s.brunelli@epartner.it>
2026-03-27 18:34:05 +0100

    fix: exclude SW and scripts from ESLint to resolve CI failures

--- eslint.config.js
+++ eslint.config.js
@@ -4,7 +4,7 @@ import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 
 export default tseslint.config(
-  { ignores: ['**/dist/**', '**/node_modules/**', '**/*.gen.ts'] },
+  { ignores: ['**/dist/**', '**/node_modules/**', '**/*.gen.ts', 'frontend/public/**', '**/scripts/**'] },
   js.configs.recommended,
   ...tseslint.configs.recommended,
   {
